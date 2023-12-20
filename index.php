d343c80989d62dbfddcc89b44840410efe2976dc
JoeJoe274 <82199206+JoeJoe274@users.noreply.github.com>
2023-12-20 10:45:37 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>My first PHP page</title>
+    </head>
+    <body>
+
+    <div style="color:steelblue; text-align:right;">
+        <h1>Heading One</h1>
+        <h2>Heading Two</h2>
+        <h3>Heading Three</h3>
+        <h4>Heading Four</h4>
+        <h5>Heading Five</h5>
+        <h6>Heading Six</h6>
+    </div>
+
+    <div style="color:yellow; font-size:50px; text-align:center;" >
+        <p>This is my first PHP page</p>
+    </div>
+
+    <?php
+    echo "My first PHP";
+    ?>
+
+    </body>
+</html>
